af5f87fcf893b8db0c9b7a0ab4805f19ab958811
Mirrodiya12 <125704692+Mirrodiya12@users.noreply.github.com>
2023-05-03 00:10:04 -0500

    Actualización DTO

---
+++ src/co/edu/uco/compuconnect/dto/NotificacionDTO.java
@@ -0,0 +1,79 @@
+package co.edu.uco.compuconnect.dto;
+
+import java.util.Date;
+import java.util.UUID;
+
+import co.edu.uco.compuconnect.crosscutting.utils.UtilDateTime;
+import co.edu.uco.compuconnect.crosscutting.utils.UtilObject;
+import co.edu.uco.compuconnect.crosscutting.utils.UtilUUID;
+
+public final class NotificacionDTO {
+	
+	private UUID identificador;
+	private ContenidoDTO contenido;
+	private Date fecha;
+	private TipoNotificacionDTO tiponotificacion;
+	
+	
+	public NotificacionDTO(UUID identificador, ContenidoDTO contenido, Date fecha,
+			TipoNotificacionDTO tiponotificacion) {
+		super();
+		setIdentificador(identificador);
+		setContenido(contenido);
+		setFecha(fecha);
+		setTiponotificacion(tiponotificacion);
+	}
+	
+	public NotificacionDTO(){
+		super();
+		setIdentificador(UtilUUID.getDefaultUuid());
+		setContenido(ReservaDTO.create());
+		setFecha(UtilDateTime.getDefaultValueDate());
+		setTiponotificacion(TipoNotificacionDTO.create());
+	}
+	
+	public static final NotificacionDTO create() {
+		return new NotificacionDTO();
+	}
+
+	public final UUID getIdentificador() {
+		return identificador;
+	}
+
+	public final NotificacionDTO setIdentificador(final UUID identificador) {
+		this.identificador = UtilUUID.getDefault(identificador);
+		return this;
+	}
+
+	public final ContenidoDTO getContenido() {
+		return contenido;
+	}
+
+	public final NotificacionDTO setContenido(final ContenidoDTO contenido) {
+		this.contenido = UtilObject.getDefault(contenido, ReservaDTO.create());
+		return this;
+	}
+
+	public final Date getFecha() {
+		return fecha;
+	}
+
+	public final NotificacionDTO setFecha(final Date fecha) {
+		this.fecha = UtilDateTime.getDefaultDate(fecha);
+		return this;
+	}
+
+	public final TipoNotificacionDTO getTiponotificacion() {
+		return tiponotificacion;
+	}
+
+	public final NotificacionDTO setTiponotificacion(final TipoNotificacionDTO tiponotificacion) {
+		this.tiponotificacion = UtilObject.getDefault(tiponotificacion, TipoNotificacionDTO.create());
+		return this;
+	}
+	
+	
+	
+	
+
+}
