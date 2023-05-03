308b7f90e5b186242dab79a2b221d524ce4e3531
SkDanielP <113185316+SkDanielP@users.noreply.github.com>
2023-05-03 01:05:48 -0500

    se agregan mas dto

--- src/co/edu/uco/compuconnect/dto/PerfilDTO.java
+++ src/co/edu/uco/compuconnect/dto/PerfilDTO.java
@@ -1,8 +1,56 @@
 package co.edu.uco.compuconnect.dto;
 
+<<<<<<< Updated upstream
 import java.util.UUID;
 
 public class PerfilDTO {
 	
 	private UUID identificador;
+=======
+import java.time.LocalTime;
+import java.util.UUID;
+
+import co.edu.uco.compuconnect.crosscutting.utils.UtilDateTime;
+import co.edu.uco.compuconnect.crosscutting.utils.UtilObject;
+import co.edu.uco.compuconnect.crosscutting.utils.UtilUUID;
+
+public class PerfilDTO {
+	
+	private UUID identificador;
+	private UsuarioDTO usuario;
+	
+	
+	public PerfilDTO(final UUID identificador, UsuarioDTO usuario) {
+		super();
+		setIdentificador(identificador);
+		setUsuario(usuario);
+	}
+	
+	public PerfilDTO() { 
+		super();
+		setIdentificador(UtilUUID.getDefaultUuid());
+		setUsuario(UsuarioDTO.create());
+	}
+	
+	public static final PerfilDTO create() {
+		return new PerfilDTO();
+	}
+	
+	public final UUID getIdentificador() {
+		return identificador;
+	}
+	public final PerfilDTO setIdentificador(final UUID identificador) {
+		this.identificador = UtilUUID.getDefault(identificador);
+		return this;
+	}
+	public final UsuarioDTO getUsuario() {
+		return usuario;
+	}
+	public final PerfilDTO setUsuario(final UsuarioDTO usuario) {
+		this.usuario = UtilObject.getDefault(usuario, UsuarioDTO.create());
+		return this;
+	}
+	
+
+>>>>>>> Stashed changes
 }
